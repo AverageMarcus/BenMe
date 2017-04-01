05df85ef6502a06a10952630d325c5b1a60c95e3
Marcus Noble <github@marcusnoble.co.uk>
2017-04-01 16:40:59 +0100

    Init

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM node:latest
+
+MAINTAINER Tomohisa Kusano <siomiz@gmail.com>
+
+ENV OPENCV_VERSION 2.4.13.2
+
+ADD https://raw.githubusercontent.com/siomiz/node-opencv/master/build.sh /build.sh
+RUN bash /build.sh \
+	&& rm /build.sh
+
+ENV LD_LIBRARY_PATH /usr/local/lib
+
+
+
+# Create app directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package.json /usr/src/app/
+RUN npm install
+
+# Bundle app source
+COPY . /usr/src/app
+
+EXPOSE 8080
+CMD [ "npm", "start" ]
---
+++ index.js
@@ -0,0 +1,71 @@
+"use strict";
+const Koa = require('koa');
+const app = new Koa();
+const cv = require('opencv');
+const jimp = require('jimp');
+const request = require('request-promise');
+
+const getBuffer = async (image) => {
+  return new Promise((resolve, reject) => {
+    image.getBuffer(jimp.MIME_JPEG, (err, im) => {
+      if (err) {
+        console.log(err);
+        return reject();
+      }
+
+      return resolve(im);
+    });
+  });
+};
+
+const getFaces = async (image) => {
+  return new Promise((resolve, reject) => {
+    cv.readImage(image, async function(err, im){
+        if (err) {
+          console.log(err);
+          return reject();
+        }
+        console.log('Loaded image in OpenCV');
+
+        im.detectObject(cv.FACE_CASCADE, {}, async function(err, faces){
+          if (err) {
+            console.log('Failed to get faces');
+            return reject();
+          }
+
+          console.log(`Got ${faces.length} faces`);
+          return resolve(faces);
+        });
+    });
+  });
+}
+
+app.use(async (ctx) => {
+  try {
+    const imageUrl = ctx.query.image;
+    if (imageUrl) {
+      console.log(`Image URL: ${imageUrl}`);
+
+      const inputImage = await jimp.read(imageUrl);
+      const benImage = await jimp.read('https://placeholder.benadventure.club/face');
+
+      const faces = await getFaces(await getBuffer(inputImage));
+
+      for (const face of faces){
+        const ben = benImage.clone();
+        console.log(face);
+        await ben.resize(face.width*2, face.height*2);
+        inputImage.composite(ben, face.x - (face.width / 2), face.y - (face.height / 2));
+      }
+
+      ctx.type = 'image/jpg';
+      ctx.body = await getBuffer(inputImage);
+    } else {
+      ctx.status = 404;
+    }
+  } catch (err) {
+    ctx.status = 500;
+  }
+});
+
+app.listen(8080);
