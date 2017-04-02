5031f112d222a91e58857bcef356e77fc996f2c5
Marcus Noble <github@marcusnoble.co.uk>
2017-04-02 06:30:38 +0100

    Added webpage

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 "use strict";
-const Koa = require('koa');
-const app = new Koa();
+const path = require('path');
 const cv = require('opencv');
 const jimp = require('jimp');
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({
+  port: process.env.PORT || 5000
+});
 
 const getBuffer = async (image) => {
   return new Promise((resolve, reject) => {
@@ -39,31 +44,56 @@ const getFaces = async (image) => {
   });
 }
 
-app.use(async (ctx) => {
-  try {
-    const imageUrl = ctx.query.image;
-    if (imageUrl) {
-      console.log(`Image URL: ${imageUrl}`);
-      const inputImage = await jimp.read(imageUrl);
-      const benImage = await jimp.read('https://placeholder.benadventure.club/face');
-      const faces = await getFaces(await getBuffer(inputImage));
-
-      for (const face of faces){
-        const ben = benImage.clone();
-        console.log(face);
-        await ben.resize(face.width*2, face.height*2);
-        inputImage.composite(ben, face.x - (face.width / 2), face.y - (face.height / 2));
-      }
-
-      ctx.type = 'image/jpg';
-      ctx.body = await getBuffer(inputImage);
-    } else {
-      ctx.status = 404;
-    }
-  } catch (err) {
-    ctx.status = 500;
+server.register(require('inert'), err => {
+  if (err) {
+    throw err;
   }
-});
 
-app.listen(process.env.PORT || 5000);
-console.log(`Listening on port ${process.env.PORT || 5000}`);
+  server.route({
+    method: 'GET',
+    path:'/',
+    handler: async (request, reply) => {
+      try {
+        const imageUrl = request.query.image;
+        if (imageUrl) {
+          console.log(`Image URL: ${imageUrl}`);
+          const inputImage = await jimp.read(imageUrl);
+          const faces = await getFaces(await getBuffer(inputImage));
+
+          for (const face of faces){
+            const ben = await jimp.read('https://placeholder.benadventure.club/face');
+            console.log(face);
+            await ben.resize(face.width*2, face.height*2);
+            inputImage.composite(ben, face.x - (face.width / 2), face.y - (face.height / 2));
+          }
+
+          return reply(await getBuffer(inputImage))
+            .type('image/jpg');
+        } else {
+          console.log(`Fetching index page`);
+          return reply.file(`./public/index.html`);
+        }
+      } catch (err) {
+        console.log(err);
+        return reply().statusCode(500);
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'public'
+        }
+    }
+  });
+
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+  });
+});
